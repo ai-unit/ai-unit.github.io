18a500d5d3b6119aab79299b7851e6fbcd6a0a82
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-08-04 23:41:08 +0200

    Experiment with GPT-2 text prediction

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,37 @@
-console.log('Hello, World!');
+const GPT_URL = 'https://gpt-rm5crmv2sq-ew.a.run.app';
+
+const TEMPERATURE = 0.7;
+const LENGTH = 30;
+
+function predict(prefix) {
+  return window.fetch(`${GPT_URL}?length=${LENGTH}&prefix=${prefix}&temperature=${TEMPERATURE}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return data.text;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+const textElems = document.querySelectorAll('main > p');
+
+for (let i = 0; textElems.length > i; i += 1) {
+  const elem = textElems[i];
+
+  predict(elem.innerText)
+    .then(text => {
+      const lastChar = text[text.length - 1];
+
+      if (lastChar === ',') {
+        text[text.length - 1] = '.';
+      } else if (lastChar !== '.') {
+        text += '.';
+      }
+
+      elem.innerText = ` ${text}`;
+    });
+}
+
