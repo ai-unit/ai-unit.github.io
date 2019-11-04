134ad58952f958e0094f9e9a457510420456afac
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-11-05 00:29:18 +0100

    Improve prediction

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,11 @@
 const GPT_URL = 'https://gpt-rm5crmv2sq-ew.a.run.app';
 
 const TEMPERATURE = 0.7;
-const LENGTH = 30;
+const LENGTH_MAX = 30;
+const LENGTH_MIN = 60;
 
-function predict(prefix) {
-  return window.fetch(`${GPT_URL}?length=${LENGTH}&prefix=${prefix}&temperature=${TEMPERATURE}&include_prefix=false`)
+function predict(prefix, length, temperature) {
+  return window.fetch(`${GPT_URL}?include_prefix=false&length=${length}&prefix=${prefix}&temperature=${temperature}`)
     .then(response => {
       return response.json();
     })
@@ -21,8 +22,9 @@ const textElems = document.querySelectorAll('main > p');
 for (let i = 0; i < 3; i += 1) {
   const elem = textElems[i];
   const prefix = elem.innerText;
+  const length = MATH_MIN + Math.floor(Math.random() * (LENGTH_MAX - LENGTH_MIN));
 
-  predict(prefix)
+  predict(prefix, length, TEMPERATURE)
     .then(text => {
       const lastChar = text[text.length - 1];
 
@@ -33,6 +35,7 @@ for (let i = 0; i < 3; i += 1) {
       }
 
       const cleanedText = text.replace(prefix, '');
+
       elem.innerHTML = `${prefix} <span class="predicted">${cleanedText}</span>`;
     });
 }
